0fa0d7df65d3f81caa4b91dad3a1605c3eb1aef2
Charlie4720 <137825941+Charlie4720@users.noreply.github.com>
2023-07-05 20:18:55 -0700

    Update change.py

--- change.py
+++ change.py
@@ -3,7 +3,8 @@ remainder_1 = float(change // 25)
 remainder_2 = float(change - 25 * remainder_1) // 10
 remainder_3 = float(change - 25 * remainder_1- 10 * remainder_2) // 5
 remainder_4 = float(change - 25 * remainder_1- 10 * remainder_2 - 5 * remainder_3)
-print("Q=", remainder_1)
-print("D=", remainder_2)
-print("N=", remainder_3)
-print("P=", remainder_4)
+
+print("Q:", remainder_1)
+print("D:", remainder_2)
+print("N:", remainder_3)
+print("P:", remainder_4)
