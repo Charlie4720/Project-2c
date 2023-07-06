72bcee09edec3788cf0a7f20c5141d90d8257697
Charlie4720 <137825941+Charlie4720@users.noreply.github.com>
2023-07-05 20:20:28 -0700

    Update change.py

--- change.py
+++ change.py
@@ -1,4 +1,4 @@
-change = float(input("Enter an amount in cents less than a dollar:"))
+change = float(input("Please enter an amount in cents less than a dollar:"))
 remainder_1 = float(change // 25)
 remainder_2 = float(change - 25 * remainder_1) // 10
 remainder_3 = float(change - 25 * remainder_1- 10 * remainder_2) // 5
