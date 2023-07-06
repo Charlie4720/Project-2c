689fdbe77163424af2392c4907f8f2bd4c85fa5f
Charlie4720 <137825941+Charlie4720@users.noreply.github.com>
2023-07-05 20:17:51 -0700

    Add files via upload

---
+++ change.py
@@ -0,0 +1,9 @@
+change = float(input("Enter an amount in cents less than a dollar:"))
+remainder_1 = float(change // 25)
+remainder_2 = float(change - 25 * remainder_1) // 10
+remainder_3 = float(change - 25 * remainder_1- 10 * remainder_2) // 5
+remainder_4 = float(change - 25 * remainder_1- 10 * remainder_2 - 5 * remainder_3)
+print("Q=", remainder_1)
+print("D=", remainder_2)
+print("N=", remainder_3)
+print("P=", remainder_4)
